334fbf277197807b9b526c56ab77ed8b9d751859
Svajunas <svajunas.plunge@gmail.com>
2024-12-04 18:35:54 +0100

    Mamad updated files added

--- Libraries/read_and_modify_landuse_lum_bck.R
+++ Libraries/read_and_modify_landuse_lum_bck.R
@@ -14,18 +14,18 @@ read_tbl <- function(tbl_name, proj_path, row_data_start, row_col_names) {
     str_trim(.) %>%
     str_split(., '[:space:]+') %>%
     unlist()
-  
+
   tbl <- vroom_lines(tbl_path, skip = row_data_start - 1) %>%
     str_trim(.) %>%
     str_split(., '\t[:space:]+|[:space:]+')
-  
+
   is_num <- tbl[[1]] %>% as.numeric() %>% suppressWarnings() %>% map_lgl(., ~!is.na(.x)) %>%  which()
-  
+
   tbl <- tbl %>%
     map(., ~ set_names(.x, col_names)) %>%
     map_df(., bind_rows) %>%
     mutate(across(all_of(is_num), ~ as.numeric(.x)))
-  
+
   return(tbl)
 }
 
@@ -37,39 +37,42 @@ lum_head <- vroom_lines(paste(proj_path, 'landuse.lum.bak', sep = '/'), n_max = 
 # Define pointers in landuse.lum ------------------------------------
 
 ## cn2
-lum$cn2[which(substr(lum$name,1,1)=='f')] <- 'rc_strow_g'
+lum$cn2[which(substr(lum$name,1,1)=='agrl')] <- 'rc_strow_g'
 lum$cn2[which(substr(lum$name,1,2)=='fr')] <- 'wood_f'
 lum$cn2[which(substr(lum$name,1,2)=='fe')] <- 'pasth'
-lum$cn2[which(substr(lum$name,1,4)=='alfa')] <- 'pasth'
-lum$cn2[which(substr(lum$name,1,4)=='agrl')] <- 'brush_f'
+lum$cn2[which(substr(lum$name,1,4)=='past')] <- 'pasth'
+lum$cn2[which(substr(lum$name,1,4)=='rngb')] <- 'brush_f'
 lum$cn2[which(substr(lum$name,1,4)=='urhd')] <- 'urban'
 lum$cn2[which(substr(lum$name,1,4)=='urld')] <- 'farm'
 lum$cn2[which(substr(lum$name,1,4)=='wetl')] <- 'pasth'
 lum$cn2[which(substr(lum$name,1,4)=='orcd')] <- 'woodgr_f'
+lum$cn2[which(substr(lum$name,1,4)=='grap')] <- 'woodgr_f'
 lum %>% filter(cn2 == "null") %>% select(name) %>% unique()
 
 ## cons_prac
-lum$cons_prac[which(substr(lum$name,1,1)=='f')] <- 'up_down_slope'
+lum$cons_prac[which(substr(lum$name,1,1)=='agrl')] <- 'up_down_slope'
 lum$cons_prac[which(substr(lum$name,1,2)=='fr')] <- 'up_down_slope'
 lum$cons_prac[which(substr(lum$name,1,2)=='fe')] <- 'up_down_slope'
-lum$cons_prac[which(substr(lum$name,1,4)=='alfa')] <- 'up_down_slope'
-lum$cons_prac[which(substr(lum$name,1,4)=='agrl')] <- 'up_down_slope'
+lum$cons_prac[which(substr(lum$name,1,4)=='past')] <- 'up_down_slope'
+lum$cons_prac[which(substr(lum$name,1,4)=='rngb')] <- 'up_down_slope'
 lum$cons_prac[which(substr(lum$name,1,4)=='urhd')] <- 'up_down_slope'
 lum$cons_prac[which(substr(lum$name,1,4)=='urld')] <- 'up_down_slope'
 lum$cons_prac[which(substr(lum$name,1,4)=='wetl')] <- 'up_down_slope'
 lum$cons_prac[which(substr(lum$name,1,4)=='orcd')] <- 'up_down_slope'
+lum$cons_prac[which(substr(lum$name,1,4)=='grap')] <- 'up_down_slope'
 lum %>% filter(cons_prac == "null") %>% select(name) %>% unique()
 
 ## ov_mann
-lum$ov_mann[which(substr(lum$name,1,1)=='f')] <- 'convtill_nores'
+lum$ov_mann[which(substr(lum$name,1,1)=='agrl')] <- 'convtill_nores'
 lum$ov_mann[which(substr(lum$name,1,2)=='fr')] <- 'forest_med'
 lum$ov_mann[which(substr(lum$name,1,2)=='fe')] <- 'densegrass'
-lum$ov_mann[which(substr(lum$name,1,4)=='alfa')] <- 'densegrass'
-lum$ov_mann[which(substr(lum$name,1,4)=='agrl')] <- 'densegrass'
+lum$ov_mann[which(substr(lum$name,1,4)=='past')] <- 'densegrass'
+lum$ov_mann[which(substr(lum$name,1,4)=='rngb')] <- 'densegrass'
 lum$ov_mann[which(substr(lum$name,1,4)=='urhd')] <- 'urban_asphalt'
 lum$ov_mann[which(substr(lum$name,1,4)=='urld')] <- 'shortgrass'
 lum$ov_mann[which(substr(lum$name,1,4)=='wetl')] <- 'densegrass'
 lum$ov_mann[which(substr(lum$name,1,4)=='orcd')] <- 'forest_light'
+lum$ov_mann[which(substr(lum$name,1,4)=='grap')] <- 'forest_light'
 lum %>% filter(ov_mann == "null") %>% select(name) %>% unique()
 
 ## urban
